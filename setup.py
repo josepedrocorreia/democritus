e902a5bc7484d96b3394992de2c63f57181a2279
Pedro <zepedro.correia@gmail.com>
2017-04-16 18:09:15 +0100

    Created setup file for packaging (issue #32).

---
+++ setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(name='democritus',
+      version='1.0.0',
+      description='Tools for working with signaling games',
+      url='https://github.com/carangorango/democritus',
+      author='José Pedro Correia',
+      author_email='zepedro.correia@gmail.com',
+      license='GPL 2.0',
+      packages=find_packages())
