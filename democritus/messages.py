c11e497bb90b89e9b29ad2805b3bb27858ccb4d6
Pedro <zepedro.correia@gmail.com>
2017-04-24 18:27:29 +0100

    Increasing coverage of tests for messages to 100% (issue #34). Adding some more input validation too.

--- democritus/messages.py
+++ democritus/messages.py
@@ -1,19 +1,26 @@
 class MessagesFactory(object):
     @staticmethod
     def create(spec):
-        if spec['type'] == 'set':
-            return MessageSet(MessageElementsFactory.create(spec['elements']))
-        else:
+        if 'type' not in spec \
+                or 'elements' not in spec:
             raise ValueError('Invalid messages specification: ' + str(spec))
+        if spec['type'] == 'set':
+            elements = MessageElementsFactory.create(spec['elements'])
+            return MessageSet(elements)
+        else:
+            raise ValueError('Unknown type in messages specification: ' + str(spec))
 
 
 class MessageElementsFactory(object):
     @staticmethod
     def create(spec):
+        if 'type' not in spec \
+                or 'size' not in spec:
+            raise ValueError('Invalid message elements specification: ' + str(spec))
         if spec['type'] == 'numbered':
             return range(1, spec['size'] + 1)
         else:
-            raise ValueError('Invalid message elements specification: ' + str(spec))
+            raise ValueError('Unknown type in message elements specification: ' + str(spec))
 
 
 class MessageSet(object):
--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,15 +1,15 @@
+import pytest
+
 from democritus.messages import *
 
-MESSAGE_ELEMENTS_SPEC_ONE_ELEMENT = {'type': 'numbered', 'size': 1}
-MESSAGE_ELEMENTS_SPEC_FIVE_ELEMENTS = {'type': 'numbered', 'size': 5}
-MESSAGES_SPEC_FIVE_ELEMENTS = {'type': 'set', 'elements': MESSAGE_ELEMENTS_SPEC_FIVE_ELEMENTS}
 
+# MessageSet
 
-def test_message_set_creation_five_elements():
-    message_set = MessageSet([1, 2, 3, 4, 5])
-    # assert message_set has elements field
-    assert type(message_set.elements) is list
-    assert message_set.elements == [1, 2, 3, 4, 5]
+def test_message_set_attributes():
+    elements = [1, 2, 3, 4, 5]
+    message_set = MessageSet(elements)
+    assert hasattr(message_set, 'elements')
+    assert message_set.elements == elements
 
 
 def test_message_set_size():
@@ -17,22 +17,48 @@ def test_message_set_size():
     assert message_set.size() == 5
 
 
-def test_message_elements_creation_one_element():
-    message_elements = MessageElementsFactory.create(MESSAGE_ELEMENTS_SPEC_ONE_ELEMENT)
-    assert type(message_elements) is list
-    assert len(message_elements) == 1
-    assert message_elements == [1]
+# MessageElementsFactory
 
-
-def test_message_elements_creation_five_elements():
-    message_elements = MessageElementsFactory.create(MESSAGE_ELEMENTS_SPEC_FIVE_ELEMENTS)
+def test_message_elements_factory_five_elements():
+    message_elements = MessageElementsFactory.create({'type': 'numbered', 'size': 5})
     assert type(message_elements) is list
-    assert len(message_elements) == 5
     assert message_elements == [1, 2, 3, 4, 5]
 
 
-def test_messages_creation_five_elements():
-    messages = MessagesFactory.create(MESSAGES_SPEC_FIVE_ELEMENTS)
+def test_message_elements_factory_unknown_type():
+    with pytest.raises(ValueError):
+        MessageElementsFactory.create({'type': '????????', 'size': 5})
+
+
+def test_message_elements_factory_missing_type():
+    with pytest.raises(ValueError):
+        MessageElementsFactory.create({'size': 2})
+
+
+def test_message_elements_factory_missing_size():
+    with pytest.raises(ValueError):
+        MessageElementsFactory.create({'type': 'numbered'})
+
+
+# MessagesFactory
+
+def test_messages_factory_five_elements():
+    messages = MessagesFactory.create({'type': 'set', 'elements': {'type': 'numbered', 'size': 5}})
     assert type(messages) is MessageSet
     assert messages.size() == 5
     assert messages.elements == [1, 2, 3, 4, 5]
+
+
+def test_messages_factory_unknown_type():
+    with pytest.raises(ValueError):
+        MessagesFactory.create({'type': '????????', 'elements': {'type': 'numbered', 'size': 5}})
+
+
+def test_messages_factory_missing_type():
+    with pytest.raises(ValueError):
+        MessagesFactory.create({'elements': {'type': 'numbered', 'size': 5}})
+
+
+def test_messages_factory_missing_elements():
+    with pytest.raises(ValueError):
+        MessagesFactory.create({'type': 'set'})
