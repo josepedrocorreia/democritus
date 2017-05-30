d622ae4f7f37c0a01f47a6acab8422b8c54a3db0
Pedro <zepedro.correia@gmail.com>
2017-05-30 17:54:05 +0100

    Added tests for replicator dynamics class (issue #34).

--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -2,8 +2,10 @@ import pytest
 
 from democritus.dynamics import *
 
-
 # Dynamics
+from democritus.messages import MessageSet
+from democritus.states import *
+
 
 def test_dynamics_update_sender():
     dynamics = Dynamics()
@@ -38,3 +40,27 @@ def test_dynamics_factory_unknown_type_raises_exception():
 def test_dynamics_factory_quantal_response_missing_rationality_raises_exception():
     with pytest.raises(ValueError):
         DynamicsFactory.create({'type': 'quantal response'})
+
+
+# ReplicatorDynamics
+
+def test_replicator_dynamics_update_sender():
+    dynamics = ReplicatorDynamics()
+    states = StateSet(['t1', 't2'], [0.6, 0.4])
+    messages = MessageSet(['m1', 'm2'])
+    utility = [[2, 0], [0, 1]]
+    sender_strategy = np.array([[0.3, 0.7], [0.4, 0.6]])
+    receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
+    new_sender_strategy = dynamics.update_sender(sender_strategy, receiver_strategy, states, messages, utility)
+    assert np.round(new_sender_strategy, decimals=3).tolist() == [[0.087, 0.913], [0.842, 0.158]]
+
+
+def test_replicator_dynamics_update_receiver():
+    dynamics = ReplicatorDynamics()
+    states = StateSet(['t1', 't2'], [0.6, 0.4])
+    messages = MessageSet(['m1', 'm2'])
+    utility = [[2, 0], [0, 1]]
+    sender_strategy = np.array([[0.3, 0.7], [0.4, 0.6]])
+    receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
+    new_receiver_strategy = dynamics.update_receiver(sender_strategy, receiver_strategy, states, messages, utility)
+    assert np.round(new_receiver_strategy, decimals=3).tolist() == [[0.36, 0.64], [0.969, 0.031]]
