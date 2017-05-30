15568f507811beb0483e947967283d5e99e021b8
Pedro <zepedro.correia@gmail.com>
2017-05-30 23:54:23 +0100

    Added tests for best response dynamics class (issue #34).

--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -64,3 +64,27 @@ def test_replicator_dynamics_update_receiver():
     receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
     new_receiver_strategy = dynamics.update_receiver(sender_strategy, receiver_strategy, states, messages, utility)
     assert np.round(new_receiver_strategy, decimals=3).tolist() == [[0.36, 0.64], [0.969, 0.031]]
+
+
+# BestResponseDynamics
+
+def test_best_response_dynamics_update_sender():
+    dynamics = BestResponseDynamics()
+    states = StateSet(['t1', 't2'], [0.6, 0.4])
+    messages = MessageSet(['m1', 'm2'])
+    utility = [[2, 0], [0, 1]]
+    sender_strategy = np.array([[0.3, 0.7], [0.4, 0.6]])
+    receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
+    new_sender_strategy = dynamics.update_sender(sender_strategy, receiver_strategy, states, messages, utility)
+    assert np.round(new_sender_strategy, decimals=3).tolist() == [[0, 1], [1, 0]]
+
+
+def test_best_response_dynamics_update_receiver():
+    dynamics = BestResponseDynamics()
+    states = StateSet(['t1', 't2'], [0.6, 0.4])
+    messages = MessageSet(['m1', 'm2'])
+    utility = [[2, 0], [0, 1]]
+    sender_strategy = np.array([[0.3, 0.7], [0.4, 0.6]])
+    receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
+    new_receiver_strategy = dynamics.update_receiver(sender_strategy, receiver_strategy, states, messages, utility)
+    assert np.round(new_receiver_strategy, decimals=3).tolist() == [[1, 0], [1, 0]]
