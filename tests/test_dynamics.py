a307ca3f1094d946899299c2a450b81f55b9983e
Pedro <zepedro.correia@gmail.com>
2017-05-31 00:21:37 +0100

    Added tests for quantal response dynamics class (closes #34).

--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -88,3 +88,27 @@ def test_best_response_dynamics_update_receiver():
     receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
     new_receiver_strategy = dynamics.update_receiver(sender_strategy, receiver_strategy, states, messages, utility)
     assert np.round(new_receiver_strategy, decimals=3).tolist() == [[1, 0], [1, 0]]
+
+
+# QuantalResponseDynamics
+
+def test_quantal_response_dynamics_update_sender():
+    dynamics = QuantalResponseDynamics(5)
+    states = StateSet(['t1', 't2'], [0.6, 0.4])
+    messages = MessageSet(['m1', 'm2'])
+    utility = [[2, 0], [0, 1]]
+    sender_strategy = np.array([[0.3, 0.7], [0.4, 0.6]])
+    receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
+    new_sender_strategy = dynamics.update_sender(sender_strategy, receiver_strategy, states, messages, utility)
+    assert np.round(new_sender_strategy, decimals=3).tolist() == [[0.001, 0.999], [0.971, 0.029]]
+
+
+def test_quantal_response_dynamics_update_receiver():
+    dynamics = QuantalResponseDynamics(5)
+    states = StateSet(['t1', 't2'], [0.6, 0.4])
+    messages = MessageSet(['m1', 'm2'])
+    utility = [[2, 0], [0, 1]]
+    sender_strategy = np.array([[0.3, 0.7], [0.4, 0.6]])
+    receiver_strategy = np.array([[0.2, 0.8], [0.9, 0.1]])
+    new_receiver_strategy = dynamics.update_receiver(sender_strategy, receiver_strategy, states, messages, utility)
+    assert np.round(new_receiver_strategy, decimals=3).tolist() == [[0.731, 0.269], [0.953, 0.047]]
