5764b282a41f344129f2d868d7c3ad7a63a69a6c
Pedro <zepedro.correia@gmail.com>
2017-07-08 00:10:45 +0100

    Moved all code to SimulationRunner object (part of issue #27).

---
+++ democritus/simulation_runner.py
@@ -0,0 +1,28 @@
+import argparse
+import numpy as np
+import time
+
+from democritus.simulation import SimulationSpecReader
+
+
+class SimulationRunner(object):
+    def __init__(self):
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument('configfile')
+        arg_parser.add_argument('--max-steps', default=100)
+        arg_parser.add_argument('--batch', action='store_true')
+        arg_parser.add_argument('--output-prefix', default=time.strftime('%Y%m%d-%H%M%S'))
+
+        self.args = arg_parser.parse_args()
+        self.simulation = SimulationSpecReader.read_from_file(self.args.configfile)
+
+    def run(self, block_at_end=True):
+        plot_steps = not self.args.batch
+        self.simulation.run_until_converged(max_steps=self.args.max_steps, plot_steps=plot_steps,
+                                            block_at_end=block_at_end)
+
+    def write_results(self):
+        sender_output_filename = self.args.output_prefix + '-sender.csv'
+        receiver_output_filename = self.args.output_prefix + '-receiver.csv'
+        np.savetxt(sender_output_filename, self.simulation.get_current_sender_strategy(), delimiter=',')
+        np.savetxt(receiver_output_filename, self.simulation.get_current_receiver_strategy(), delimiter=',')
--- vagueness-games.py
+++ vagueness-games.py
@@ -1,24 +1,5 @@
-import argparse
-import time
+from democritus.simulation_runner import SimulationRunner
 
-import numpy as np
-
-from democritus.simulation import SimulationSpecReader
-
-# Read arguments
-argparser = argparse.ArgumentParser()
-argparser.add_argument('--batch', action='store_true')
-argparser.add_argument('configfile')
-argparser.add_argument('--output-prefix', default=time.strftime('%Y%m%d-%H%M%S'))
-args = argparser.parse_args()
-batch_mode = args.batch
-
-# Run
-simulation = SimulationSpecReader.read_from_file(args.configfile)
-simulation.run_until_converged(max_steps=1, plot_steps=not batch_mode, block_at_end=True)
-
-# Output to file
-sender_output_filename = args.output_prefix + '-sender.csv'
-receiver_output_filename = args.output_prefix + '-receiver.csv'
-np.savetxt(sender_output_filename, simulation.get_current_sender_strategy(), delimiter=',')
-np.savetxt(receiver_output_filename, simulation.get_current_receiver_strategy(), delimiter=',')
+runner = SimulationRunner()
+runner.run(block_at_end=False)
+runner.write_results()
